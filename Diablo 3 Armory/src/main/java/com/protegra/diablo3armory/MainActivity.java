48201db013b80c917b487e03350b83a3b4c83c8c
David <davidrempel6@gmail.com>
2014-04-26 23:35:16 -0500

    add non-working web services

--- Diablo 3 Armory/src/main/java/com/protegra/diablo3armory/MainActivity.java
+++ Diablo 3 Armory/src/main/java/com/protegra/diablo3armory/MainActivity.java
@@ -1,5 +1,9 @@
 package com.protegra.diablo3armory;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,16 +15,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
@@ -28,19 +29,16 @@ import java.net.URL;
 import java.util.Scanner;
 
 
-public class MainActivity extends ActionBarActivity
-{
+public class MainActivity extends ActionBarActivity {
     public final static String EXTRA_MESSAGE = "com.protegra.diablo3armory.MESSAGE";
     private static final int CONNECTION_TIMEOUT = 10000;
-    private static final int DATARETRIEVAL_TIMEOUT = 10000;
+    private static final int DATA_RETRIEVAL_TIMEOUT = 10000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null)
-        {
+        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
@@ -49,17 +47,15 @@ public class MainActivity extends ActionBarActivity
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        
+    public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -72,40 +68,45 @@ public class MainActivity extends ActionBarActivity
 
     /**
      * takes the entered search text and finds the hero
+     *
      * @param view
      */
-    public void searchHero(View view) throws IOException
-    {
+    public void searchHero(View view) throws IOException {
         EditText editText = (EditText) findViewById(R.id.hero_text);
         String battleTag = editText.getText().toString();
 
         // instead of hard coding the host we should use a radio button to choose host
-        getHero("http://us.battle.net", battleTag);
+        if (isNetworkAvailable()) {
+            getCareer("http://us.battle.net", battleTag);
+        }
     }
 
-    private void getHero(String host, String hero) throws IOException
-    {
+    private void getCareer(String host, String hero) throws IOException {
         disableConnectionReuseIfNecessary();
+        enableHttpResponseCache();
 
         String serviceUrl = host + "/api/d3/profile/" + hero + "/";
 
         HttpURLConnection urlConnection = null;
-        try
-        {
+        try {
             // create connection
             URL urlToRequest = new URL(serviceUrl);
             urlConnection = (HttpURLConnection) urlToRequest.openConnection();
             urlConnection.setConnectTimeout(CONNECTION_TIMEOUT);
-            urlConnection.setReadTimeout(DATARETRIEVAL_TIMEOUT);
+            urlConnection.setReadTimeout(DATA_RETRIEVAL_TIMEOUT);
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setInstanceFollowRedirects(true);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("Content-length", "0");
+
+            urlConnection.connect();
 
             // handle issues
             int statusCode = urlConnection.getResponseCode();
-            if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED)
-            {
+            if (statusCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
                 // handle unauthorized (if service requires user login)
-            }
-            else if (statusCode != HttpURLConnection.HTTP_OK)
-            {
+            } else if (statusCode != HttpURLConnection.HTTP_OK) {
                 // handle any other errors, like 404, 500,..
             }
 
@@ -114,35 +115,23 @@ public class MainActivity extends ActionBarActivity
 
             JSONObject jObject = new JSONObject(getResponseText(in));
 
-            if (jObject.get("code").equals("NOTFOUND"))
-            {
+            if (jObject.get("code").equals("NOTFOUND")) {
                 // bad battleTag
-            }
-            else
-            {
+            } else {
                 // good battleTag
             }
-        }
-        catch (MalformedURLException e)
-        {
+
+        } catch (MalformedURLException e) {
             // URL is invalid
-        }
-        catch (SocketTimeoutException e)
-        {
+        } catch (SocketTimeoutException e) {
             // data retrieval or connection timed out
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             // could not read response body
             // (could not create input stream)
-        } catch (JSONException e)
-        {
+        } catch (JSONException e) {
             // response body is no valid JSON string
-        }
-        finally
-        {
-            if (urlConnection != null)
-            {
+        } finally {
+            if (urlConnection != null) {
                 urlConnection.disconnect();
             }
         }
@@ -151,36 +140,69 @@ public class MainActivity extends ActionBarActivity
     /**
      * required in order to prevent issues in earlier Android version.
      */
-    private static void disableConnectionReuseIfNecessary()
-    {
+    private static void disableConnectionReuseIfNecessary() {
         // see HttpURLConnection API doc
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO)
-        {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO) {
             System.setProperty("http.keepAlive", "false");
         }
     }
 
-    private static String getResponseText(InputStream inStream)
-    {
+    private static String getResponseText(InputStream inStream) {
         // very nice trick from
         // http://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
         return new Scanner(inStream).useDelimiter("\\A").next();
     }
 
+    private void enableHttpResponseCache() {
+        try {
+            long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
+            File httpCacheDir = new File(getCacheDir(), "http");
+            Class.forName("android.net.http.HttpResponseCache")
+                    .getMethod("install", File.class, long.class)
+                    .invoke(null, httpCacheDir, httpCacheSize);
+        } catch (Exception httpResponseCacheNotAvailable) {
+        }
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        // if no network is available networkInfo will be null
+        // otherwise check if we are connected
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment
-    {
-        public PlaceholderFragment()
-        {
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
+    }
+}
+
+class CareerDownload extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+
+        String battleTag = (String) objects[0];
+
+        if (Career.hasSavedCareer(battleTag)) {
+            return Career.getSavedCareer(battleTag);
+        } else {
+            // this is where the web request should be.
+        }
+
+        return null;
     }
 }
